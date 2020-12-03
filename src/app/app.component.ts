942085dec890b83d0d1b75347d97f4bd4591724a
AminePolak <usualpolak@hotmail.com>
2020-12-03 09:07:42 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, Subscription } from 'rxjs';
+import { timeout } from 'rxjs-compat/operator/timeout';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+  secondes: number;
+  counterSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
+  
+}
